7ae06296b231a1109830e64db00340819d8c7eb7
fernandoACF28 <fernando.costa@fisica.ufmt.br>
2025-05-28 10:39:50 -0300

    packpage update version 1

---
+++ __pycache__/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+with open('__pycache__/requirements.txt') as f:
+    requirements = f.read().splitlines()
+
+setup(
+    name="OMI_functions",
+    version="0.1",
+    packages=find_packages(),
+    install_requires=requirements,  # Dependências (adicione aqui se precisar)
+    author="Fernando Fernandes",
+    author_email="fernando.allysson@usp.br",
+    description="This is packpage with utils functions for make extract temporal series from hdf_files from OMI product",
+    url="https://github.com/fernandoACF28/OMI",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+    ],
+)
