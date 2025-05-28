996b1077d6636216518f6e605a3cf41fb9d72089
fernandoACF28 <fernando.costa@fisica.ufmt.br>
2025-05-28 12:21:19 -0300

    update functions

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open('requirements.txt') as f:
     requirements = f.read().splitlines()
 
 setup(
-    name="OMI_functions",
+    name="omi_functions",
     version="0.1",
     packages=find_packages(),
     install_requires=requirements,  # Dependências (adicione aqui se precisar)
